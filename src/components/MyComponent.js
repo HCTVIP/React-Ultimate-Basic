013a419ebb1c06a3c5eba0c97e18c383223996a6
TaiHC1 <teo@gmail.com>
2022-12-05 23:35:21 +0700

    video 18

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -9,11 +9,19 @@ class MyComponent extends React.Component {
     age: 26,
   };
 
+  handleClick(e) {
+    console.log(e.clientX);
+  }
+  handleMouseOver(e) {
+    console.log(e.clientY);
+  }
   // JSX cho phép viết code java script bên trong code của HTML
   render() {
     return (
       <div>
         My name is {this.state.name} and i from {this.state.address}
+        <button onClick={this.handleClick}>Click me!</button>
+        <button onMouseOver={this.handleMouseOver}>Click me!</button>
       </div>
     );
   }
