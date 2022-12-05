b216bfa6688e64ae9fc1bf81e04f759f10138edf
TaiHC1 <teo@gmail.com>
2022-12-05 23:07:43 +0700

    video 17

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,9 +3,19 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "HaryPhamDev",
+    address: "hoidanit",
+    age: 26,
+  };
+
   // JSX cho phép viết code java script bên trong code của HTML
   render() {
-    return <div>{Math.random()}</div>;
+    return (
+      <div>
+        My name is {this.state.name} and i from {this.state.address}
+      </div>
+    );
   }
 }
 
