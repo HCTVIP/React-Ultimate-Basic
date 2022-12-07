ba1e482ad8d72e8d786a7585eea93525ecc3785f
Tai <hoangchitai08c1a@gmail.com>
2022-12-07 18:45:36 +0700

    stateless and statefull

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -3,46 +3,44 @@ import React from "react";
 import './DisplayInfor.scss'
 import logo from '../logo.svg';
 
-class DisplayInfor extends React.Component {
-  constructor(props) {
-    console.log('call contructor 1:');
-    super(props);
-    this.state = {
-      show: true,
-    }
-  }
 
-  componentDidMount(){
-    console.log("call me component did mount");
-    setTimeout(() => {
-      document.title = "Bim Anh"
-    },3000)
-  }
+// stateless vs statefull
+// class DisplayInfor extends React.Component {
+//   render() {
+//     console.log('call me render')
+//     const { listUsers } = this.props;
+//     // props => viet tat properties
+//     return (
+//       <div className="display-infor-container">
+//         {true && 
+//           <>
+//             {listUsers.map((user) => {
+//                 return (
+//                   <div key={user.id}>
+//                     <div  className={+user.age > 18 ? "green" : "red"}>
+//                       <div>
+//                         <div>My name's{user.name}</div>
+//                         <div>My age's{user.age}</div>
+//                       </div>
+//                       <div>
+//                         <button onClick={() => {this.props.handleDeleteUser(user.id)}}>Delete</button>
+//                       </div>
+//                     </div>
+//                   </div>
+//                 );
+//             })}
+//           </>
+//         }
+//       </div>
+//     );
+//   }
+// }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log("call me component did Update", this.props, prevProps);
-    if(this.props.listUsers !== prevProps.listUsers) {
-      if(this.props.listUsers.length === 5) {
-        alert("Please dung")
-      }
-    }
-  }
-
-  handleShowHide() {
-    this.setState({
-      show: !this.state.show
-    })
-  }
-  
-  render() {
-    console.log('call me render')
-    const { listUsers } = this.props;
-    // props => viet tat properties
+const DisplayInfor = (props) => {
+  const { listUsers } = props;
     return (
       <div className="display-infor-container">
-        {/* <img src={logo} className='img'/> */}
-        <span onClick={() => {this.handleShowHide()}}>{this.state.show === true ? 'Hide' : 'Show'} list User:</span>
-        {this.state.show && 
+        {true && 
           <>
             {listUsers.map((user) => {
                 return (
@@ -63,7 +61,8 @@ class DisplayInfor extends React.Component {
         }
       </div>
     );
-  }
 }
+
+
 
 export default DisplayInfor;
