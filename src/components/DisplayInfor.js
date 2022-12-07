5da1504ed962871ff8a9f89f24fe38a3305d21f1
Tai <hoangchitai08c1a@gmail.com>
2022-12-07 16:26:33 +0700

    CSS với React bằng thư viện SASS

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,5 +1,6 @@
 /* eslint-disable array-callback-return */
 import React from "react";
+import './DisplayInfor.scss'
 
 class DisplayInfor extends React.Component {
   state = {
@@ -10,28 +11,26 @@ class DisplayInfor extends React.Component {
       show: !this.state.show
     })
   }
-
+  
   render() {
     const { listUsers } = this.props;
     // pops => viet tat properties
     return (
-      <>
-              <div>
-                <span onClick={() => {this.handleShowHide()}}>{this.state.show === true ? 'Hide' : 'Show'} list User:</span>
-              </div>
+      <div className="display-infor-container">
+        <span onClick={() => {this.handleShowHide()}}>{this.state.show === true ? 'Hide' : 'Show'} list User:</span>
         {listUsers.map((user) => {
             return (
-              <>
-              {this.state.show && 
-              <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                <div>My name's{user.name}</div>
-                <div>My age's{user.age}</div>
-                <hr/>
-              </div>}
-              </>
+              <div key={user.id}>
+                {this.state.show && 
+                <div  className={+user.age > 18 ? "green" : "red"}>
+                  <div>My name's{user.name}</div>
+                  <div>My age's{user.age}</div>
+                  <hr/>
+                </div>}
+              </div>
             );
         })}
-      </>
+      </div>
     );
   }
 }
