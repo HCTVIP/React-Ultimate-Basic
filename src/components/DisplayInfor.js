95886aca8f65e7b6016103421128783d8072b2b8
Tai <hoangchitai08c1a@gmail.com>
2022-12-07 23:08:38 +0700

    useEffect

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,40 +1,7 @@
 /* eslint-disable array-callback-return */
-import React, { useState } from "react";
-import './DisplayInfor.scss'
-// import logo from '../logo.svg';
+import React, { useEffect, useState } from "react";
+import './DisplayInfor.scss';
 
-
-// stateless vs statefull
-// class DisplayInfor extends React.Component {
-//   render() {
-//     console.log('call me render')
-//     const { listUsers } = this.props;
-//     // props => viet tat properties
-//     return (
-//       <div className="display-infor-container">
-//         {true && 
-//           <>
-//             {listUsers.map((user) => {
-//                 return (
-//                   <div key={user.id}>
-//                     <div  className={+user.age > 18 ? "green" : "red"}>
-//                       <div>
-//                         <div>My name's{user.name}</div>
-//                         <div>My age's{user.age}</div>
-//                       </div>
-//                       <div>
-//                         <button onClick={() => {this.props.handleDeleteUser(user.id)}}>Delete</button>
-//                       </div>
-//                     </div>
-//                   </div>
-//                 );
-//             })}
-//           </>
-//         }
-//       </div>
-//     );
-//   }
-// }
 
 const DisplayInfor = (props) => {
   const { listUsers } = props;
@@ -44,6 +11,15 @@ const DisplayInfor = (props) => {
   const handleShowHideListUser = () => {
     setShowHideListUser(!isShowHideListUser)
   }
+  console.log("call me render 1")
+
+
+  useEffect (() => {
+    if(listUsers.length === 0) {
+      alert("Please select")
+    }
+  },[listUsers])
+  
 
     return (
       <div className="display-infor-container">
