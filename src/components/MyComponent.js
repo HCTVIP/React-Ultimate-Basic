f71a7dfa3ac0b7c6c422d469787cc08ff97271b0
TaiHC1 <teo@gmail.com>
2022-12-07 13:20:06 +0700

    1

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -10,18 +10,50 @@ class MyComponent extends React.Component {
   };
 
   handleClick(e) {
-    console.log(e.clientX);
+    // Merge class
+    this.setState({
+      age: `${Math.floor(Math.random() * 100 + 1)}`,
+      name: "Eric",
+    });
   }
-  handleMouseOver(e) {
-    console.log(e.clientY);
+
+  handleOnChangeInput(e) {
+    console.log(e.target.value);
+    this.setState({
+      name: e.target.value,
+    });
   }
+
+  handleChangeValue(e) {
+    e.preventDefault();
+    alert("me");
+  }
+
   // JSX cho phép viết code java script bên trong code của HTML
   render() {
     return (
       <div>
-        My name is {this.state.name} and i from {this.state.address}
-        <button onClick={this.handleClick}>Click me!</button>
-        <button onMouseOver={this.handleMouseOver}>Click me!</button>
+        My name is {this.state.name} and i {this.state.age}
+        <button
+          onClick={(e) => {
+            this.handleClick(e);
+          }}
+        >
+          Click me!
+        </button>
+        <form
+          onSubmit={(e) => {
+            this.handleChangeValue(e);
+          }}
+        >
+          <input
+            type="text"
+            onChange={(e) => {
+              this.handleOnChangeInput(e);
+            }}
+          />
+          <button>Submit</button>
+        </form>
       </div>
     );
   }
