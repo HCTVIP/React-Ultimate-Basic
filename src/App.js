c24c17a9ae95e67f019f4072cdead32c4f1df73d
Tai <hoangchitai08c1a@gmail.com>
2022-12-08 13:53:47 +0700

    start project

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-// import logo from "./logo.svg";
-import React from "react";
+
 import "./App.scss";
-// import { useDispatch, useSelector } from "react-redux";
-// import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent";
-// const App = () => {
-//   const count = useSelector((state) => state.counter.count);
-//   const dispatch = useDispatch();
-class App extends React.Component {
-  render() {
+import Header from "./components/Header/Header";
+
+
+const App = () =>{
+  
     return (
-      <div>
-        <MyComponent />
+      <div className="app-container">
+        <Header />
       </div>
-      // <div className="App">
-      //   <header className="App-header">
-      //     <img src={logo} className="App-logo" alt="logo" />
-      //     <p>Hello World!</p>
-      //     <div>Count = {count}</div>
-      //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-      //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      //   </header>
-      // </div>
     );
   }
-}
 
 export default App;
